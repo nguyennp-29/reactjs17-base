07a02cb6b570753f5de9979368718e37390770c6
Nguyen Ngo Phuc <nguyennp@younetgroup.com>
2022-05-06 13:32:55 +0700

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import App from './App'
+import './index.css'
+
+const appRoot = document.getElementById('root')
+
+ReactDOM.render(
+	<React.StrictMode>
+		<App />
+	</React.StrictMode>,
+	appRoot,
+)
